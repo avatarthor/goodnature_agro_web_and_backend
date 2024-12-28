93c40232baa02aeb3adf79256835b493a304851e
Kauma Mbewe <kaynerd26@gmail.com>
2024-12-28 15:05:39 +0200

    Define table name and update fillable fields in FarmerInput model

--- Modules/FarmerInputs/Models/FarmerInput.php
+++ Modules/FarmerInputs/Models/FarmerInput.php
@@ -5,13 +5,22 @@ namespace Modules\FarmerInputs\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use App\Models\Farmer;
+use Modules\FarmerInputs\Models\InputType; // Import the InputType model
 
 
 class FarmerInput extends Model
 {
     use HasFactory;
 
-    protected $guarded = [];
+    // Define the table name
+    protected $table = 'farmer_inputs_module';
+
+    protected $fillable = [
+        'farmer_id',
+        'input_type_id',
+        'quantity',
+        'distributed_date',
+    ];
 
     public function farmer()
     {
